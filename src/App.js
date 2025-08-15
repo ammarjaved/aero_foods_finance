77e066f949e3640421b682856993cef47966e5c5
ammar <you@example.com>
2025-08-15 20:44:18 +0800

    changes added

--- src/App.js
+++ src/App.js
@@ -12,11 +12,29 @@ import Timesheet from "./components/time-sheet/Timesheet";
 import TimesheetABE from "./com_abe/time-sheet-abe/TimesheetABE";
 import TimesheetAmazon from "./com_amz/time-sheet-amz/TimesheetAmazon";
 import DailyWastage from "./components/wastage/DailyWastage";
+import DailyWastageYus from "./com_abe/wastage/DailyWastage";
+import DailyWastageAmz from "./com_amz/wastage/DailyWastage";
+
+
 import DailyBankReconciliation from "./components/bank-reconciliation/DailyBankReconciliation";
+import DailyBankReconciliationYus from "./com_abe/bank-reconciliation/DailyBankReconciliation";
+import DailyBankReconciliationAmz from "./com_amz/bank-reconciliation/DailyBankReconciliation";
+
+
 import MonthlyMaterials from "./components/materials/MonthlyMaterials";
+import MonthlyMaterialsYus from "./com_abe/materials/MonthlyMaterials";
+import MonthlyMaterialsAmz from "./com_amz/materials/MonthlyMaterials";
+
+
 import StockIn from "./components/stock-in/StockIn";
+import StockInYus from "./com_abe/stock-in/StockIn";
+import StockInAmz from "./com_amz/stock-in/StockIn";
+
 import Summary from "./components/summary/Summary";
 import Expenses from "./components/expense/Expense";
+import ExpensesYus from "./com_abe/expense/Expense";
+import ExpensesAmz from "./com_amz/expense/Expense";
+
 
 function App() {
   return (
@@ -28,17 +46,33 @@ function App() {
         <Route path="/landing-amz" element={<LandingAmz />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard-yus" element={<DashboardYus />} />
-                <Route path="/dashboard-amz" element={<DashboardAmz />} />
+        <Route path="/dashboard-amz" element={<DashboardAmz />} />
 
         <Route path="/timesheet" element={<Timesheet />} />
         <Route path="/wastage" element={<DailyWastage />} />
+        <Route path="/wastage-yus" element={<DailyWastageYus />} />
+        <Route path="/wastage-amz" element={<DailyWastageAmz />} />
+
+
         <Route path="/reconciliation" element={<DailyBankReconciliation />} />
+        <Route path="/reconciliation-yus" element={<DailyBankReconciliationYus />} />
+        <Route path="/reconciliation-amz" element={<DailyBankReconciliationAmz />} />
+
         <Route path="/materials" element={<MonthlyMaterials />} />
+        <Route path="/materials-yus" element={<MonthlyMaterialsYus />} />
+        <Route path="/materials-amz" element={<MonthlyMaterialsAmz />} />
+
         <Route path="/stockin" element={<StockIn />} />
+        <Route path="/stockin-yus" element={<StockInYus />} />
+        <Route path="/stockin-amz" element={<StockInAmz />} />
+
         <Route path="/summary" element={<Summary />} />
         <Route path="/TimesheetABE" element={<TimesheetABE />} />
         <Route path="/TimesheetAmazon" element={<TimesheetAmazon />} />
         <Route path="/Expenses" element={<Expenses />} />
+        <Route path="/Expenses-yus" element={<ExpensesYus />} />
+        <Route path="/Expenses-amz" element={<ExpensesAmz />} />
+
 
 
 
